334721f9d73364e1c895c7a2c79dbcdd332ff525
Berkay ALTINER <berkay.altiner@st.uskudar.edu.tr>
2021-02-21 14:28:25 +0200

    Connection established between DataAcces and Business layers

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,11 @@ namespace Business.Abstract
 {
     public interface ICarService
     {
+        List<Car> GetAll();
+        List<Car> GeyById(int carId);
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
+
     }
 }
