f8c314c717a04332f0717ee90c3ea553d598f53f
Berkay ALTINER <berkay.altiner@st.uskudar.edu.tr>
2021-02-21 03:06:38 +0200

    Implemented operations

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -1,11 +1,36 @@
 ﻿using DataAccess.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
 {
-    public class InMemoryCarDal:ICarDal
+    public class InMemoryCarDal : ICarDal
     {
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
