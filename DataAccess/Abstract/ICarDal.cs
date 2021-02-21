3c3b47225f95b7dcad79674bb7fa15001bc2590b
Berkay ALTINER <berkay.altiner@st.uskudar.edu.tr>
2021-02-21 03:05:01 +0200

    Modified to interface type

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public class ICarDal
+    public interface ICarDal
     {
+        List<Car> GetAll();
+        void Add (Car car);
+        void Delete(Car car);
+        void Update(Car car);
+
+        List<Car> GetById(int carId);
+
     }
 }
