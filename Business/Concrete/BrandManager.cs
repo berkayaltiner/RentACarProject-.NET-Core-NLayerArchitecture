b68bf0ab2aa54e682487763a23ede0e3741f88c6
berkayaltiner <berkay.altiner.98@gmail.com>
2021-03-06 15:37:12 +0200

    Create BrandManager.cs
    Business operations and injection of Data Access layer were applied.

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal) // Injection data access layer.
+        {
+            _brandDal = brandDal;
+        }
+
+        public void Add(Brand brand)
+        {
+            _brandDal.Add(brand);
+        }
+
+        public void Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+        }
+
+        public Brand GetById(int brandId)
+        {
+            return _brandDal.Get(b=>b.BrandId == brandId);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brandDal.GetAll();
+        }
+
+        public void Update(Brand brand)
+        {
+            _brandDal.Update(brand);
+        }
+    }
+}
