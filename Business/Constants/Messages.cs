e45688510f0d925204c5b1513ba2b58a43f13e6d
berkayaltiner <berkay.altiner.98@gmail.com>
2021-04-01 17:24:55 +0300

    Messages classes created.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Car Added.";
+        public static string CarNotAdded = "Car didn't add.";
+        public static string CarDeleted = "Car Deleted.";
+        public static string CarNotDeleted = "Car didn't delete.";
+        public static string CarUpdated = "Car Updated.";
+        public static string CarNotUpdated = "Car didn't updated.";
+        public static string CarNameInvalid = "Car Name Invalid !";
+        public static string CarsListed = "Cars Listed.";
+        public static string CarNotListed = "Car didn't list.";
+
+        public static string BrandAdded = "Brand Added.";
+        public static string BrandNotAdded = "Brand didn't add.";
+        public static string BrandUpdate = "Brand Updated";
+        public static string BrandNotUpdate = "Brand didn't update";
+        public static string BrandsListed = "Brand Listed.";
+        public static string BrandNotListed = "Brand didn't list.";
+        public static string BrandDeleted = "Brand Deleted.";
+        public static string BrandNotDeleted = "Brand didn't delete.";
+
+        public static string ColorAdded = "Color Added.";
+        public static string ColorNotAdded = "Color didn't added.";
+        public static string ColorUpdate = "Color Updated.";
+        public static string ColorNotUpdate = "Color didn't update.";
+        public static string ColorListed = "Color Listed.";
+        public static string ColorNotListed = "Color didn't list.";
+        public static string ColorDeleted = "Color Deleted.";
+        public static string ColorNotDeleted = "Color didn't delete.";
+
+        public static string UserAdded = "User added.";
+        public static string UserNotAdded = "User didn't add.";
+        public static string UserDeleted = "User Deleted.";
+        public static string UserNotDeleted = "User didn't delete.";
+        public static string UserUpdated = "User updated..";
+        public static string UserNotUpdated = "User didn't update.";
+        public static string UsersListed = "User listed.";
+        public static string UserNotListed = "User didn't list.";
+
+        public static string CustomerAdded = "Customer added.";
+        public static string CustomerNotAdded = "Customer didn't add.";
+        public static string CustomerDeleted = "Customer deleted.";
+        public static string CustomerNotDeleted = "Customer didn't delete.";
+        public static string CustomerUpdated = "Customer updated.";
+        public static string CustomerNotUpdated = "Customer didn't update.";
+        public static string CustomersListed = "Customer listed.";
+        public static string CustomersNotListed = "Customer didn't list.";
+
+        public static string RentalAdded = "Rental Added.";
+        public static string RentalNotAdded = "Rental didn't add.";
+        public static string RentalAddedError = "Car can't be rented because of not returning back.";
+        public static string RentalUpdated = "Rental Updated";
+        public static string RentalNotUpdated = "Rental didn't updated.";
+        public static string RentalListed = "Rental listed.";
+        public static string RentalNotListed = "Rental didn't list.";
+        public static string RentalDeleted = "Rental Deleted";
+        public static string RentalNotDeleted = "Rental didn't delete.";
+
+        public static string MaintenanceTime = "Maintenance Time";
+    }
+}
